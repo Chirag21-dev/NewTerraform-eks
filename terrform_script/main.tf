adf6097ec37141292232ffbb22ee4467f50e2db4
Chirag21-dev <chiragks21@outlook.com>
2025-03-13 08:03:13 +0000

    Added all the files related to terrform.

---
+++ terrform_script/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
